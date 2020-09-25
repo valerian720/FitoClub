35e472c8957991547445c66a7dc70cf6978478c7
Valerian <valerian7202@gmail.com>
2020-09-26 01:54:12 +0300

    index change attempt

--- producer/views.py
+++ producer/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
+from django.core import serializers
+from django.http import JsonResponse
+
+from producer.models import * 
 
 def index(request):
     if request.user.is_authenticated:
-        context = {}
+        producers = Producer.objects.all()
+
+        context = {"qq": serializers.serialize('json', list(producers), fields=('name','product_types')) }
         return render(request, 'index.html', context)
     else:
         return redirect('/login', permanent=False)
